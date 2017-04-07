a5e761fce5f59e1a18a985c28393fdb5dad97c28
Hong SoonIl <ghosdi0624@naver.com>
2017-04-07 13:21:55 +0900

    no message

--- src/main/java/kr/co/foot/controller/MymapController.java
+++ src/main/java/kr/co/foot/controller/MymapController.java
@@ -364,7 +364,7 @@ public class MymapController {
       if(compareCheckPointidx != null){
          for(int i=0; i<compareCheckPointidx.size(); i++){
             if(checkpointidx == compareCheckPointidx.get(i).getCheckpointidx()){
-               return "�̹� ��ϵǾ� �ֽ��ϴ�.";
+               return "already registed";
             }
          }
       }
@@ -375,7 +375,7 @@ public class MymapController {
       favoriteplaceVO.setPlacename(placename);
       
       mymapService.insertFavoritePlace(favoriteplaceVO);
-      return "��� ���� �Ϸ�";
+      return "register complete";
    }
    
    @RequestMapping("/map/getFavoriteMap.do")
@@ -393,7 +393,7 @@ public class MymapController {
       if(compareRegmapidx != null){
          for(int i=0; i<compareRegmapidx.size(); i++){
             if(getRegmap.getIdx() == compareRegmapidx.get(i).getRegmapidx()){
-               return "�̹� ��ϵǾ� �ֽ��ϴ�.";
+               return "already registed";
             }
          }
       }
@@ -402,7 +402,7 @@ public class MymapController {
       favoritemapVO.setUserid(userid);
       mymapService.insertFavoriteMap(favoritemapVO);
       
-      return "�� ���� �Ϸ�";
+      return "register complete";
    }
    
    
